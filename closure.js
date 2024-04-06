3ec416880383896b8c1f4a02c31baed9df00f424
NextGithub90 <akunbeta958@gmail.com>
2024-04-06 10:59:33 +0700

    materi closure

--- closure.js
+++ closure.js
@@ -1,12 +1,40 @@
+// test ke 1
 function waktuHari(waktu) {
   return function (bangun) {
     console.log("kamu bangun pada " + waktu + " pukul " + bangun);
   };
 }
-let waktuPagi = waktuHari("pagi hari");
-let wakktuSiang = waktuHari("siang hari");
-let wakktuMalam = waktuHari("malam hari");
+waktuHari("7")("pagi hari");
+waktuHari("12")("siang hari hari");
 
-waktuPagi("7");
-wakktuSiang("8");
-wakktuMalam("9");
+// let waktuPagi = waktuHari("pagi hari");
+// let wakktuSiang = waktuHari("siang hari");
+// let wakktuMalam = waktuHari("malam hari");
+
+// waktuPagi("7");
+// wakktuSiang("8");
+// wakktuMalam("9");
+
+// test ke 2
+// function add() {
+//   let counter = 0;
+//   return function () {
+//     return ++counter;
+//   };
+// }
+// counter = 10;
+// let a = add();
+// console.log(a());
+// console.log(a());
+// console.log(a());
+// console.log(a());
+
+// let add = (function () {
+//   let counter = 0;
+//   return function () {
+//     return ++counter;
+//   };
+// })();
+// console.log(add());
+// console.log(add());
+// console.log(add());
